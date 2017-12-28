58e3a150f55d4ffaffdc6dad6a7e15616925855a
Humberto A. Sainz <SAINZHA@innovasi.com>
2017-12-28 15:02:28 -0800

    Use DevTools when creating the store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,6 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from '../reducers/rootReducer';
 
 export default function configureStore(preLoadedState) {
@@ -8,7 +9,7 @@ export default function configureStore(preLoadedState) {
 
     const storeEnhancers = [middlewareEnhancer];
     
-    const composedEnhancer = compose(...storeEnhancers);
+    const composedEnhancer = composeWithDevTools(...storeEnhancers);
 
     const store = createStore(
         rootReducer,
