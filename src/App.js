555a0d4897f96bb29f87b6e6fc5c3ed85ecb0262
Humberto A. Sainz <SAINZHA@innovasi.com>
2017-12-29 10:42:24 -0800

    Initial HardCoded Tab Menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-import SampleComponent from "./SampleComponent";
+import React, { Component } from 'react';
+import { Container, Header, Menu } from 'semantic-ui-react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Project Mini-Mek</h2>
+          <Header inverted as="h1">Project Mini-Mek</Header>
         </div>
-        <SampleComponent />
+        <Container>
+          <Menu tabular>
+            <Menu.Item name="unitInfo" active={true}>Unit Info</Menu.Item>
+            <Menu.Item name="pilots" active={false}>Pilots</Menu.Item>
+            <Menu.Item name="mechs" active={false}>Mechs</Menu.Item>
+            <Menu.Item name="unitOrganization" active={false}>Unit Organization</Menu.Item>
+          </Menu>
+        </Container>
       </div>
     );
   }
