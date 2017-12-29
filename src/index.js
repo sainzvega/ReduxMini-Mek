b35b3a71e8226b0bcbea9903fae6e56c7d7ef274
Humberto A. Sainz <SAINZHA@innovasi.com>
2017-12-29 10:15:40 -0800

    Configured HMR for React Tree Components

--- src/index.js
+++ src/index.js
@@ -2,15 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
-import App from "./App";
 import "./index.css";
 
 import configureStore from "./store/configureStore";
 const store = configureStore();
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+const rootEl = document.getElementById("root");
+
+let render = () => {
+
+  const App = require("./App").default;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootEl
+  );
+};
+
+if(process.env.NODE_ENV !== "production") {
+  if(module.hot) {
+    module.hot.accept("./App", () => {
+      setTimeout(render);
+    });
+  }
+}
+
+render();
