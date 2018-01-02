d86b134e1486e224ac105f15905287a8a47afab5
Humberto A. Sainz <SAINZHA@innovasi.com>
2018-01-02 14:30:14 -0800

    Added missing selector{

--- src/features/tabs/TabBarContainer.jsx
+++ src/features/tabs/TabBarContainer.jsx
@@ -1,16 +1,17 @@
-import {connect} from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import TabBar from "./TabBar";
+import TabBar from "common/components/TabBar";
 
-import {selectCurrentTab} from "./tabsSelectors";
-import {selectTab} from "./tabsActions";
+import { selectCurrentTab } from "./tabsSelectors";
+import { selectTab } from "./tabsActions";
 
-const mapState = (state) => {
-    const currentTab = selectCurrentTab(state);
+const mapState = state => {
+  const currentTab = selectCurrentTab(state);
 
-    return {currentTab};
-}
+  return { currentTab };
+};
 
-const actions = {onTabClick : selectTab};
+const actions = { onTabClick: selectTab };
 
 export default connect(mapState, actions)(TabBar);
