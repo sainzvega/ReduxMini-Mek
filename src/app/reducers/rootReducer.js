bbe284ec97680e6509bf20bdee9f50e661e93aec
Humberto A. Sainz <SAINZHA@innovasi.com>
2018-01-03 13:28:09 -0800

    Add logic for tracking the currently selcted pilot

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,11 +1,13 @@
 import {combineReducers} from "redux";
 
 import entitiesReducer from "./entitiesReducer";
+import pilotsReducer from "features/pilots/pilotsReducer";
 import tabsReducer from "features/tabs/tabsReducer";
 import unitInfoReducer from "features/unitInfo/unitInfoReducer";
 
 const rootReducer = combineReducers({
     entities : entitiesReducer,
+    pilots: pilotsReducer,
     unitInfo: unitInfoReducer,
     tabs : tabsReducer,    
 });
