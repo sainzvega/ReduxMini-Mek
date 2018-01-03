7f49d724efaa6650f3f6f0ab0f2710bb675d80ee
Humberto A. Sainz <SAINZHA@innovasi.com>
2018-01-03 13:20:06 -0800

    Connect the Mechs component to display a list of mechs from the the store

--- src/features/mechs/Mechs.jsx
+++ src/features/mechs/Mechs.jsx
@@ -1,49 +1,55 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import orm from "app/orm";
 
-import {
-    Grid,
-    Segment,
-    Header,
-} from "semantic-ui-react";
-
+import { Grid, Segment, Header } from "semantic-ui-react";
 import MechsList from "./MechsList";
 import MechDetails from "./MechDetails";
 
-const mechs = [
-    {
-        id : 1,
-        name : "Warhammer",
-        type : "WHM-6R",
-        weight : 70,
-    }
-];
+const mapState = state => {
+  const session = orm.session(state.entities);
+  const { Mech } = session;
 
-class Mechs extends Component {
-    state = {
-        mechs : mechs,
-    }
+  const mechs = Mech.all()
+    .toModelArray()
+    .map(mechModel => {
+      const mech = {
+        ...mechModel.ref,
+        mechType: {}
+      };
 
-    render() {
-        const {mechs} = this.state;
+      if (mechModel.type) {
+        mech.mechType = { ...mechModel.type.ref };
+      }
 
-        const currentMech = mechs[0] || {};
+      return mech;
+    });
 
-        return (
+  return { mechs };
+};
+
+export class Mechs extends Component {
+  render() {
+    const { mechs = [] } = this.props;
+
+    const currentMech = mechs[0] || {};
+
+    return (
+      <Segment>
+        <Grid>
+          <Grid.Column width={10}>
+            <Header as="h3">Mechs List</Header>
+            <MechsList mechs={mechs} />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Header as="h3">Mech Details</Header>
             <Segment>
-                <Grid>
-                    <Grid.Column width={10}>
-                        <Header as="h3">Mechs List</Header>
-                        <MechsList mechs={mechs}/>
-                    </Grid.Column>
-                    <Grid.Column width={6}>
-                        <Header as="h3">Mech Details</Header>
-                        <Segment >
-                            <MechDetails mech={currentMech} />
-                        </Segment>
-                    </Grid.Column>
-                </Grid>
+              <MechDetails mech={currentMech} />
             </Segment>
-        );
-    }
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    );
+  }
 }
-export default Mechs;
+export default connect(mapState)(Mechs);
--- src/features/pilots/Pilots.jsx
+++ src/features/pilots/Pilots.jsx
@@ -1,66 +1,84 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import {
-    Grid,
-    Segment,
-    Header,
-} from "semantic-ui-react";
+import { Grid, Segment, Header } from "semantic-ui-react";
 
 import orm from "app/orm";
 
 import PilotsList from "./PilotsList";
 import PilotDetails from "./PilotDetails";
 
+const mapState = state => {
+  // Create a Redux-ORM Session from our "entities" slice, which
+  // contains the "tables" for each model type
+  const session = orm.session(state.entities);
 
-const mapState = (state) => {
-    // Create a Redux-ORM Session from our "entities" slice, which
-    // contains the "tables" for each model type
-    const session =  orm.session(state.entities);
+  // Retrieve the model class that we need.  Each Session
+  // specifically "binds" model classes to itself, so that
+  // updates to model instances are applied to that session.
+  // These "bound classes" are available as fields in the sesssion.
+  const { Pilot } = session;
 
-    // Retrieve the model class that we need.  Each Session
-    // specifically "binds" model classes to itself, so that
-    // updates to model instances are applied to that session.
-    // These "bound classes" are available as fields in the sesssion.
-    const {Pilot} = session;
+  // Query the session for all Pilot instances.
+  // The QuerySet that is returned from all() can be used to
+  // retrieve instances of the Pilot class, or retrieve the
+  // plain JS objects that are actually in the store.
 
-    // Query the session for all Pilot instances.
-    // The QuerySet that is returned from all() can be used to
-    // retrieve instances of the Pilot class, or retrieve the
-    // plain JS objects that are actually in the store.
-    // The toRefArray() method will give us an array of the
-    // plain JS objects for each item in the QuerySet.
-    const pilots = Pilot.all().toRefArray();
+  // The toModelArray() method will give us an array of the
+  // Model class instances objects for each item in the QuerySet.
+  const pilots = Pilot.all()
+    .toModelArray()
+    .map(pilotModel => {
+      // Access the underlying plain JS object using the "ref" field,
+      // and make a shallow copy of it
+      const pilot = {
+        ...pilotModel.ref
+      };
 
-    // Now that we have an array of all pilot objects, return it as a prop
-    return {pilots};
-}
+      // We want to look up pilotModel.mech.mechType.  Just in case the
+      // relational fields are null, we'll do a couple safety checks as we go.
 
+      // Look up the associated Mech instance using the foreign-key
+      // field that we defined in the Pilot Model class
+      const { mech } = pilotModel;
+
+      // If there actually is an associated mech, include the
+      // mech type's ID as a field in the data passed to the component
+      if (mech && mech.type) {
+        pilot.mechType = mech.type.id;
+      }
+
+      return pilot;
+    });
+
+  // Now that we have an array of all pilot objects, return it as a prop
+  return { pilots };
+};
 
 export class Pilots extends Component {
-    render() {
-        const {pilots = []} = this.props;
+  render() {
+    const { pilots = [] } = this.props;
 
-        // Use the first pilot as the "current" one for display, if available.
-        const currentPilot = pilots[0] || {};
+    // Use the first pilot as the "current" one for display, if available.
+    const currentPilot = pilots[0] || {};
 
-        return (
+    return (
+      <Segment>
+        <Grid>
+          <Grid.Column width={10}>
+            <Header as="h3">Pilot List</Header>
+            <PilotsList pilots={pilots} />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Header as="h3">Pilot Details</Header>
             <Segment>
-                <Grid>
-                    <Grid.Column width={10}>
-                        <Header as="h3">Pilot List</Header>
-                        <PilotsList pilots={pilots} />
-                    </Grid.Column>
-                    <Grid.Column width={6}>
-                        <Header as="h3">Pilot Details</Header>
-                        <Segment >
-                            <PilotDetails pilot={currentPilot} />
-                        </Segment>
-                    </Grid.Column>
-                </Grid>
+              <PilotDetails pilot={currentPilot} />
             </Segment>
-        );
-    }
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    );
+  }
 }
 
 export default connect(mapState)(Pilots);
