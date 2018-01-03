f08ea5628967b8aad898c706c7eb44b37c16433b
Humberto A. Sainz <SAINZHA@innovasi.com>
2018-01-03 11:55:26 -0800

    Add Mech and MechDesign model class

--- src/app/orm.js
+++ src/app/orm.js
@@ -1,7 +1,9 @@
-import {ORM} from "redux-orm";
-import Pilot from "features/pilots/Pilot"
+import { ORM } from "redux-orm";
+import Pilot from "features/pilots/Pilot";
+import Mech from "features/mechs/Mech";
+import MechDesign from "features/mechs/MechDesign";
 
 const orm = new ORM();
-orm.register(Pilot);
+orm.register(Mech, MechDesign, Pilot);
 
 export default orm;
