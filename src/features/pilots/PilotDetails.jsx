de3ca0a7d11dffbe1fd609e97eb16514af110ddb
Humberto A. Sainz <SAINZHA@innovasi.com>
2018-01-03 16:16:50 -0800

    Add start/stop editing buttons to PilotDetails

--- src/features/pilots/PilotDetails.jsx
+++ src/features/pilots/PilotDetails.jsx
@@ -1,104 +1,124 @@
 import React from "react";
-import {connect} from "react-redux";
-import {Form, Dropdown} from "semantic-ui-react";
-
+import { connect } from "react-redux";
+import { Form, Dropdown, Grid, Button } from "semantic-ui-react";
 
 import orm from "app/orm";
-import {selectCurrentPilot} from "./pilotsSelectors";
+
+import { selectCurrentPilot, selectIsEditingPilot } from "./pilotsSelectors";
+
+import { startEditingPilot, stopEditingPilot } from "./pilotsActions";
 
 const RANKS = [
-    {value: "Private", text : "Private"},
-    {value: "Corporal", text : "Corporal"},
-    {value: "Sergeant", text : "Sergeant"},
-    {value: "Lieutenant", text : "Lieutenant"},
-    {value: "Captain", text : "Captain"},
-    {value: "Major", text : "Major"},
-    {value: "Colonel", text : "Colonel"},
+  { value: "Private", text: "Private" },
+  { value: "Corporal", text: "Corporal" },
+  { value: "Sergeant", text: "Sergeant" },
+  { value: "Lieutenant", text: "Lieutenant" },
+  { value: "Captain", text: "Captain" },
+  { value: "Major", text: "Major" },
+  { value: "Colonel", text: "Colonel" }
 ];
 
-const MECHS = [
-    {value : "WHM-6R", text : "Warhammer WHM-6R"}
-];
+const MECHS = [{ value: "WHM-6R", text: "Warhammer WHM-6R" }];
 
-const mapState = (state) => {
-    let pilot;
+const mapState = state => {
+  let pilot;
 
-    const currentPilot = selectCurrentPilot(state);
+  const currentPilot = selectCurrentPilot(state);
 
-    const session = orm.session(state.entities);
-    const {Pilot} = session;
+  const session = orm.session(state.entities);
+  const { Pilot } = session;
 
-    if(Pilot.hasId(currentPilot)) {
-        pilot = Pilot.withId(currentPilot).ref;
-    }
+  if (Pilot.hasId(currentPilot)) {
+    pilot = Pilot.withId(currentPilot).ref;
+  }
 
-    return {pilot}
-}
+  const pilotIsSelected = Boolean(currentPilot);
+  const isEditingPilot = selectIsEditingPilot(state);
 
-export const PilotDetails = ({pilot={}}) =>{
-    const {
-        name = "",
-        rank = "",
-        age = "",
-        gunnery = "",
-        piloting = "",
-        mechType = "",
-    } = pilot;
+  return { pilot, pilotIsSelected, isEditingPilot };
+};
 
-    return (
-        <Form size="large">
-            <Form.Field name="name" width={16}>
-                <label>Name</label>
-                <input
-                    placeholder="Name"
-                    value={name}
-                    disabled={true}
-                />
-            </Form.Field>
-            <Form.Field name="rank" width={16}>
-                <label>Rank</label>
-                <Dropdown
-                    fluid
-                    selection
-                    options={RANKS}
-                    value={rank}
-                    disabled={true}
-                />
-            </Form.Field>
-            <Form.Field name="age" width={6}>
-                <label>Age</label>
-                <input
-                    placeholder="Age"
-                    value={age}
-                    disabled={true}
-                />
-            </Form.Field>
-            <Form.Field name="gunnery" width={6}>
-                <label>Gunnery</label>
-                <input
-                    value={gunnery}
-                    disabled={true}
-                />
-            </Form.Field>
-            <Form.Field name="piloting" width={6}>
-                <label>Piloting</label>
-                <input
-                    value={piloting}
-                    disabled={true}
-                />
-            </Form.Field>
-            <Form.Field name="mech" width={16}>
-                <label>Mech</label>
-                <Dropdown
-                    fluid
-                    selection
-                    options={MECHS}
-                    value={mechType}
-                    disabled={true}
-                />
-            </Form.Field>
-        </Form>
-    );
-}
+const actions = {
+  startEditingPilot,
+  stopEditingPilot
+};
 
-export default connect(mapState)(PilotDetails);
+const PilotDetails = ({
+  pilot = {},
+  pilotIsSelected = false,
+  isEditingPilot = false,
+  ...actions
+}) => {
+  const {
+    name = "",
+    rank = "",
+    age = "",
+    gunnery = "",
+    piloting = "",
+    mechType = ""
+  } = pilot;
+
+  const canStartEditing = pilotIsSelected && !isEditingPilot;
+  const canStopEditing = pilotIsSelected && isEditingPilot;
+
+  return (
+    <Form size="large">
+      <Form.Field name="name" width={16}>
+        <label>Name</label>
+        <input placeholder="Name" value={name} disabled={!canStopEditing} />
+      </Form.Field>
+      <Form.Field name="rank" width={16}>
+        <label>Rank</label>
+        <Dropdown
+          fluid
+          selection
+          options={RANKS}
+          value={rank}
+          disabled={!canStopEditing}
+        />
+      </Form.Field>
+      <Form.Field name="age" width={6}>
+        <label>Age</label>
+        <input placeholder="Age" value={age} disabled={!canStopEditing} />
+      </Form.Field>
+      <Form.Field name="gunnery" width={6}>
+        <label>Gunnery</label>
+        <input value={gunnery} disabled={!canStopEditing} />
+      </Form.Field>
+      <Form.Field name="piloting" width={6}>
+        <label>Piloting</label>
+        <input value={piloting} disabled={!canStopEditing} />
+      </Form.Field>
+      <Form.Field name="mech" width={16}>
+        <label>Mech</label>
+        <Dropdown
+          fluid
+          selection
+          options={MECHS}
+          value={mechType}
+          disabled={true}
+        />
+      </Form.Field>
+      <Grid.Row width={16}>
+        <Button
+          primary
+          disabled={!canStartEditing}
+          type="button"
+          onClick={actions.startEditingPilot}
+        >
+          Start Editing
+        </Button>
+        <Button
+          secondary
+          disabled={!canStopEditing}
+          type="button"
+          onClick={actions.stopEditingPilot}
+        >
+          Stop Editing
+        </Button>
+      </Grid.Row>
+    </Form>
+  );
+};
+
+export default connect(mapState, actions)(PilotDetails);
