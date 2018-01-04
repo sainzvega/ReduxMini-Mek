f75bab04e4ca4e0b2df6f0b7eece2d6095e76cc7
Humberto A. Sainz <SAINZHA@innovasi.com>
2018-01-03 16:10:09 -0800

    Add logic to track if pulot is being edited

--- src/features/pilots/pilotsReducer.js
+++ src/features/pilots/pilotsReducer.js
@@ -1,24 +1,48 @@
-import { createReducer } from "common/utils/reducerUtils";
+import {createReducer} from "common/utils/reducerUtils";
 
-import { PILOT_SELECT } from "./pilotsConstants";
+import {
+    PILOT_SELECT,
+    PILOT_EDIT_START,
+    PILOT_EDIT_STOP,
+} from "./pilotsConstants";
 
 const initialState = {
-  currentPilot: null
+    currentPilot : null,
+    isEditing : false,
 };
-
 export function selectPilot(state, payload) {
-  const prevSelectedPilot = state.currentPilot;
-  const newSelectedPilot = payload.currentPilot;
+    const prevSelectedPilot = state.currentPilot;
+    const newSelectedPilot = payload.currentPilot;
 
-  const isSamePilot = prevSelectedPilot === newSelectedPilot;
+    const isSamePilot = prevSelectedPilot === newSelectedPilot;
 
-  return {
-    // Deselect entirely if it's a second click on the same pilot,
-    // otherwise go ahead and select the one that was clicked
-    currentPilot: isSamePilot ? null : newSelectedPilot
-  };
+    return {
+        ...state,
+        // Deselect entirely if it's a second click on the same pilot,
+        // otherwise go ahead and select the one that was clicked
+        currentPilot : isSamePilot ? null : newSelectedPilot,
+        // Any time we select a different pilot, we stop editing
+        isEditing : false,
+    };
 }
 
+export function startEditingPilot(state, payload) {
+    return {
+        ...state,
+        isEditing : true,
+    };
+}
+
+export function stopEditingPilot(state, payload) {
+    return {
+        ...state,
+        isEditing : false,
+    };
+}
+
+
 export default createReducer(initialState, {
-  [PILOT_SELECT]: selectPilot
+    [PILOT_SELECT] : selectPilot,
+    [PILOT_EDIT_START] : startEditingPilot,
+    [PILOT_EDIT_STOP] : stopEditingPilot,
 });
