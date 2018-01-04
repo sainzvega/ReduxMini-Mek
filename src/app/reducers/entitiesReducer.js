a854c276ba38238c60f954a5c45cd7a31b513204
Humberto A. Sainz <SAINZHA@innovasi.com>
2018-01-04 09:54:56 -0800

    Clear out existing models on load to avoid conflicts when reloading data

--- src/app/reducers/entitiesReducer.js
+++ src/app/reducers/entitiesReducer.js
@@ -1,29 +1,34 @@
-import {createReducer} from "common/utils/reducerUtils";
+import { createReducer } from "common/utils/reducerUtils";
 
-import orm from "app/orm"
+import orm from "app/orm";
 
-import {DATA_LOADED} from "features/tools/toolConstants";
+import { DATA_LOADED } from "features/tools/toolConstants";
 
-
-const initialState = orm.getEmptyState()
+const initialState = orm.getEmptyState();
 
 export function loadData(state, payload) {
-    // Create a Redux-ORM session from our entities "database tables" object
-    const session = orm.session(state);
-    // Get a reference to the correct version of model classes for this Session
-    const {Pilot, MechDesign, Mech} = session;
+  // Create a Redux-ORM session from our entities "database tables" object
+  const session = orm.session(state);
+  // Get a reference to the correct version of model classes for this Session
+  const { Pilot, MechDesign, Mech } = session;
 
-    const {pilots, designs, mechs} = payload;
+  const { pilots, designs, mechs } = payload;
 
-    // Insert the data entries into the Session
-    pilots.forEach(pilot => Pilot.parse(pilot));
-    designs.forEach(design => MechDesign.parse(design));
-    mechs.forEach(mech => Mech.parse(mech));
+  [Pilot, Mech, MechDesign].forEach(modelType => {
+    modelType
+      .all()
+      .toModelArray()
+      .forEach(model => model.delete());
+  });
+  // Insert the data entries into the Session
+  pilots.forEach(pilot => Pilot.parse(pilot));
+  designs.forEach(design => MechDesign.parse(design));
+  mechs.forEach(mech => Mech.parse(mech));
 
-    // return a new version of the entities state object with the inserted entries
-    return session.state;
+  // return a new version of the entities state object with the inserted entries
+  return session.state;
 }
 
 export default createReducer(initialState, {
-    [DATA_LOADED] : loadData
+  [DATA_LOADED]: loadData
 });
