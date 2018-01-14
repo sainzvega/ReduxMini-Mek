5bd2e3452d797171a41ec73d49946cf39d89f1d4
Humberto Sainz <hsainz@talentize.com>
2018-01-14 12:10:50 -0800

    Form change handling

--- src/features/pilots/PilotDetails.jsx
+++ src/features/pilots/PilotDetails.jsx
@@ -1,166 +1,212 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Form, Dropdown, Grid, Button } from "semantic-ui-react";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {Form, Dropdown, Grid, Button} from "semantic-ui-react";
+
+import FormEditWrapper from "common/components/FormEditWrapper";
 
 import orm from "app/orm";
 
-import { selectCurrentPilot, selectIsEditingPilot } from "./pilotsSelectors";
+import {
+    selectCurrentPilot,
+    selectIsEditingPilot
+} from "./pilotsSelectors";
 
-import { startEditingPilot, stopEditingPilot } from "./pilotsActions";
+import {updateEntity} from "features/entities/entityActions";
+
+import {getValueFromEvent} from "common/utils/clientUtils";
+
+
+import {
+    startEditingPilot,
+    stopEditingPilot,
+} from "./pilotsActions";
+
 
 const RANKS = [
-  { value: "Private", text: "Private" },
-  { value: "Corporal", text: "Corporal" },
-  { value: "Sergeant", text: "Sergeant" },
-  { value: "Lieutenant", text: "Lieutenant" },
-  { value: "Captain", text: "Captain" },
-  { value: "Major", text: "Major" },
-  { value: "Colonel", text: "Colonel" }
+    {value: "Private", text : "Private"},
+    {value: "Corporal", text : "Corporal"},
+    {value: "Sergeant", text : "Sergeant"},
+    {value: "Lieutenant", text : "Lieutenant"},
+    {value: "Captain", text : "Captain"},
+    {value: "Major", text : "Major"},
+    {value: "Colonel", text : "Colonel"},
 ];
 
 const SKILL_VALUES = [
-  { value: 0, text: 0 },
-  { value: 1, text: 1 },
-  { value: 2, text: 2 },
-  { value: 3, text: 3 },
-  { value: 4, text: 4 },
-  { value: 5, text: 5 },
-  { value: 6, text: 6 },
-  { value: 7, text: 7 },
-  { value: 8, text: 8 }
+    {value : 0, text : 0},
+    {value : 1, text : 1},
+    {value : 2, text : 2},
+    {value : 3, text : 3},
+    {value : 4, text : 4},
+    {value : 5, text : 5},
+    {value : 6, text : 6},
+    {value : 7, text : 7},
+    {value : 8, text : 8},
+]
+
+const MECHS = [
+    {value : "WHM-6R", text : "Warhammer WHM-6R"}
 ];
 
-const MECHS = [{ value: "WHM-6R", text: "Warhammer WHM-6R" }];
+const mapState = (state) => {
+    let pilot;
 
-const mapState = state => {
-  let pilot;
+    const currentPilot = selectCurrentPilot(state);
 
-  const currentPilot = selectCurrentPilot(state);
+    const session = orm.session(state.entities);
+    const {Pilot} = session;
 
-  const session = orm.session(state.entities);
-  const { Pilot } = session;
+    if(Pilot.hasId(currentPilot)) {
+        pilot = Pilot.withId(currentPilot).ref;
+    }
 
-  if (Pilot.hasId(currentPilot)) {
-    pilot = Pilot.withId(currentPilot).ref;
-  }
+    const pilotIsSelected = Boolean(currentPilot);
+    const isEditingPilot = selectIsEditingPilot(state);
 
-  const pilotIsSelected = Boolean(currentPilot);
-  const isEditingPilot = selectIsEditingPilot(state);
 
-  return { pilot, pilotIsSelected, isEditingPilot };
-};
+    return {pilot, pilotIsSelected, isEditingPilot}
+}
 
 const actions = {
-  startEditingPilot,
-  stopEditingPilot
-};
-
-export class PilotDetails extends Component {
-  render() {
-    const {
-      pilot = {},
-      pilotIsSelected = false,
-      isEditingPilot = false
-    } = this.props;
-    const { startEditingPilot, stopEditingPilot } = this.props;
-
-    const {
-      name = "",
-      rank = "",
-      age = "",
-      gunnery = "",
-      piloting = "",
-      mechType = ""
-    } = pilot;
-
-    const canStartEditing = pilotIsSelected && !isEditingPilot;
-    const canStopEditing = pilotIsSelected && isEditingPilot;
-
-    return (
-      <Form size="large">
-        <Form.Field
-          name="name"
-          label="Name"
-          width={16}
-          placeholder="Name"
-          value={name}
-          disabled={!canStopEditing}
-          control="input"
-        />
-        <Form.Field
-          name="rank"
-          label="Rank"
-          width={16}
-          control={Dropdown}
-          fluid
-          selection
-          options={RANKS}
-          value={rank}
-          disabled={!canStopEditing}
-        />
-        <Form.Field
-          name="age"
-          width={6}
-          label="Age"
-          placeholder="Age"
-          control="input"
-          value={age}
-          disabled={!canStopEditing}
-        />
-        <Form.Field
-          name="gunnery"
-          label="Gunnery"
-          width={6}
-          control={Dropdown}
-          fluid
-          selection
-          options={SKILL_VALUES}
-          value={gunnery}
-          disabled={!canStopEditing}
-        />
-        <Form.Field
-          name="piloting"
-          label="Piloting"
-          width={6}
-          control={Dropdown}
-          fluid
-          selection
-          options={SKILL_VALUES}
-          value={piloting}
-          disabled={!canStopEditing}
-        />
-        <Form.Field
-          name="mech"
-          label="Mech"
-          width={16}
-          control={Dropdown}
-          fluid
-          selection
-          options={MECHS}
-          value={mechType}
-          disabled={true}
-        />
-        <Grid.Row width={16}>
-          <Button
-            primary
-            disabled={!canStartEditing}
-            type="button"
-            onClick={startEditingPilot}
-          >
-            Start Editing
-          </Button>
-          <Button
-            secondary
-            disabled={!canStopEditing}
-            type="button"
-            onClick={stopEditingPilot}
-          >
-            Stop Editing
-          </Button>
-        </Grid.Row>
-      </Form>
-    );
-  }
+    startEditingPilot,
+    stopEditingPilot,
+    updateEntity,
 }
+
+export class PilotDetails  extends Component {
+
+    onInputChanged = (e) => {
+        const newValues = getValueFromEvent(e);
+        const {id} = this.props.pilot;
+
+        this.props.updateEntity("Pilot", id, newValues);
+    }
+
+    onDropdownChanged = (e, result) => {
+        const {name, value} = result;
+        const newValues = { [name] : value};
+        const {id} = this.props.pilot;
+
+        this.props.updateEntity("Pilot", id, newValues);
+    }
+
+    render() {
+        const {pilot={}, pilotIsSelected = false, isEditingPilot = false } = this.props;
+        const {startEditingPilot, stopEditingPilot} = this.props;
+
+        const {
+            name = "",
+            rank = "",
+            age = "",
+            gunnery = "",
+            piloting = "",
+            mechType = "",
+        } = pilot;
+
+        const canStartEditing = pilotIsSelected && !isEditingPilot;
+        const canStopEditing = pilotIsSelected && isEditingPilot;
+
+        return (
+            <Form size="large">
+                <FormEditWrapper
+                    singleValue={true}
+                    value={ {name} }
+                    onChange={this.onInputChanged}
+                    passIsEditing={false}
+                >
+                    <Form.Field
+                        name="name"
+                        label="Name"
+                        width={16}
+                        placeholder="Name"
+                        disabled={!canStopEditing}
+                        control="input"
+                    />
+                </FormEditWrapper>
+                <Form.Field
+                    name="rank"
+                    label="Rank"
+                    width={16}
+                    control={Dropdown}
+                    fluid
+                    selection
+                    options={RANKS}
+                    value={rank}
+                    onChange={this.onDropdownChanged}
+                    disabled={!canStopEditing}
+                />
+                <FormEditWrapper
+                    singleValue={true}
+                    value={ {age} }
+                    onChange={this.onInputChanged}
+                    passIsEditing={false}
+                >
+                    <Form.Field
+                        name="age"
+                        width={6}
+                        label="Age"
+                        placeholder="Age"
+                        control="input"
+                        disabled={!canStopEditing}
+                    />
+                </FormEditWrapper>
+                <Form.Field
+                    name="gunnery"
+                    label="Gunnery"
+                    width={6}
+                    control={Dropdown}
+                    fluid
+                    selection
+                    options={SKILL_VALUES}
+                    value={gunnery}
+                    onChange={this.onDropdownChanged}
+                    disabled={!canStopEditing}
+                />
+                <Form.Field
+                    name="piloting"
+                    label="Piloting"
+                    width={6}
+                    control={Dropdown}
+                    fluid
+                    selection
+                    options={SKILL_VALUES}
+                    value={piloting}
+                    onChange={this.onDropdownChanged}
+                    disabled={!canStopEditing}
+                />
+                <Form.Field
+                    name="mech"
+                    label="Mech"
+                    width={16}
+                    control={Dropdown}
+                    fluid
+                    selection
+                    options={MECHS}
+                    value={mechType}
+                    disabled={true}
+                />
+                <Grid.Row width={16}>
+                    <Button
+                        primary
+                        disabled={!canStartEditing}
+                        type="button"
+                        onClick={startEditingPilot}
+                    >
+                        Start Editing
+                    </Button>
+                    <Button
+                        secondary
+                        disabled={!canStopEditing}
+                        type="button"
+                        onClick={stopEditingPilot}
+                    >
+                        Stop Editing
+                    </Button>
+                </Grid.Row>
+            </Form>
+        );
+    }
+}
+
 
 export default connect(mapState, actions)(PilotDetails);
--- src/features/unitInfo/UnitInfo.jsx
+++ src/features/unitInfo/UnitInfo.jsx
@@ -1,72 +1,82 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Form, Dropdown, Segment } from "semantic-ui-react";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {Form, Dropdown, Segment} from "semantic-ui-react";
+
+import FormEditWrapper from "common/components/FormEditWrapper";
 
 import {getValueFromEvent} from "common/utils/clientUtils";
 
-import { selectUnitInfo } from "./unitInfoSelectors";
-import { updateUnitInfo } from "./unitInfoActions";
+import {selectUnitInfo} from "./unitInfoSelectors";
+import {updateUnitInfo} from "./unitInfoActions";
 
 const FACTIONS = [
-  { value: "cc", text: "Capellan Confederation" },
-  { value: "dc", text: "Draconis Combine" },
-  { value: "fs", text: "Federated Suns" },
-  { value: "fwl", text: "Free Worlds League" },
-  { value: "lc", text: "Lyran Commonwealth" },
-  { value: "wd", text: "Wolf's Dragoons" }
+    {value : "cc", text : "Capellan Confederation"},
+    {value : "dc", text : "Draconis Combine"},
+    {value : "fs", text : "Federated Suns"},
+    {value : "fwl", text : "Free Worlds League"},
+    {value : "lc", text : "Lyran Commonwealth"},
+    {value : "wd", text : "Wolf's Dragoons"}
 ];
 
-const mapState = state => ({
-  unitInfo: selectUnitInfo(state)
+
+const mapState = (state) => ({
+    unitInfo : selectUnitInfo(state),
 });
 
 const actions = {
-  updateUnitInfo
+    updateUnitInfo,
 };
 
 class UnitInfo extends Component {
-  onAffiliationChanged = (e, result) => {
-    const { name, value } = result;
+    onAffiliationChanged = (e, result) => {
+        const {name, value} = result;
 
-    const newValues = { [name]: value };
-    this.props.updateUnitInfo(newValues);
-  };
-
-  onNameChanged = e => {
-    const newValues = getValueFromEvent(e);
+        const newValues = { [name] : value};
         this.props.updateUnitInfo(newValues);
-  };
+    }
 
-  render() {
-    const { unitInfo = {} } = this.props;
-    const { name, affiliation } = unitInfo;
+    onNameChanged = (e) => {
+        const newValues = getValueFromEvent(e);
+        this.props.updateUnitInfo(newValues);
+    }
 
-    return (
-      <Segment attached="bottom">
-        <Form size="large">
-          <Form.Field name="name" width={6}>
-            <label>Unit Name</label>
-            <input
-              placeholder="Name"
-              name="name"
-              value={name}
-              onChange={this.onNameChanged}
-            />
-          </Form.Field>
-          <Form.Field name="affiliation" width={6}>
-            <label>Affiliation</label>
-            <Dropdown
-              name="affiliation"
-              selection
-              options={FACTIONS}
-              value={affiliation}
-              onChange={this.onAffiliationChanged}
-            />
-          </Form.Field>
-        </Form>
-      </Segment>
-    );
-  }
+
+    render() {
+        const {unitInfo = {}, updateUnitInfo} = this.props;
+        const {name, affiliation} = unitInfo;
+
+        return (
+            <Segment attached="bottom">
+                <Form size="large">
+                    <Form.Field name="name" width={6} >
+                        <label>Unit Name</label>
+                        <FormEditWrapper
+                            singleValue={true}
+                            value={ {name} }
+                            onChange={updateUnitInfo}
+                            passIsEditing={false}
+                        >
+                            <input
+                                placeholder="Name"
+                                name="name"
+                            />
+                        </FormEditWrapper>
+                    </Form.Field>
+                    <Form.Field name="affiliation" width={6}>
+                        <label>Affiliation</label>
+                        <Dropdown
+                            name="affiliation"
+                            selection
+                            options={FACTIONS}
+                            value={affiliation}
+                            onChange={this.onAffiliationChanged}
+                        />
+                    </Form.Field>
+                </Form>
+            </Segment>
+        );
+    }
 }
+
 
 export default connect(mapState, actions)(UnitInfo);
